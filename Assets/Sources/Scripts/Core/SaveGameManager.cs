1c4b17a979eebc76569ef41f11bdf7c838717062
Kirill Minaev <kv_l@ukr.net>
2020-02-23 15:10:58 +0200

    Added core ui

---
+++ Assets/Sources/Scripts/Core/SaveGameManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Sources.Scripts.Core
+{
+    public class SaveGameManager
+    {
+        private const string GameSavedStorage = "storage.gamesaved";
+
+        private bool _gameSaved;
+        
+        public void Init()
+        {
+            LoadGameSaved();
+        }
+
+        private void LoadGameSaved()
+        {
+            _gameSaved = PlayerPrefs.HasKey(GameSavedStorage);
+        }
+
+        public bool GameSaved => _gameSaved;
+        
+        //Singleton
+        private static SaveGameManager _instance;
+        public static SaveGameManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new SaveGameManager();
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
--- Assets/Sources/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Sources/Scripts/MainMenu/MainMenuManager.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+using Sources.Scripts.Core;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MainMenuManager : MonoBehaviour
+namespace Sources.Scripts.MainMenu
 {
-    // Start is called before the first frame update
-    void Start()
+    public class MainMenuManager : MonoBehaviour
     {
-        
-    }
+        public Button mainMenuButton;
+        public Button newGameButton;
+        public Button loadGameButton;
+        public Button quitButton;
 
-    // Update is called once per frame
-    void Update()
-    {
+        public List<GameObject> objectsToEnable;
         
+        private void Awake()
+        {
+            mainMenuButton.onClick.AddListener(OnMainMenuButtonClick);
+            newGameButton.onClick.AddListener(OnNewGameButtonClick);
+            loadGameButton.onClick.AddListener(OnLoadGameButtonClick);
+            quitButton.onClick.AddListener(OnQuitButtonClick);
+
+            loadGameButton.interactable = SaveGameManager.Instance.GameSaved;
+        }
+
+        private void OnQuitButtonClick()
+        {
+            Application.Quit();
+        }
+
+        private void OnLoadGameButtonClick()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnNewGameButtonClick()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OnMainMenuButtonClick()
+        {
+            foreach (var objectToEnable in objectsToEnable)
+            {
+                objectToEnable.SetActive(!objectToEnable.activeSelf);
+            }
+        }
     }
 }
